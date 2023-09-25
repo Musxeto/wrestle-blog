2bb66c55b819ee085402d8c42c3d7c08d3467918
Musxeto <mustafamalikawan786@gmail.com>
2023-09-25 15:01:30 +0500

    Uploading the files

---
+++ main.py
@@ -0,0 +1,200 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+import json
+import os
+import math
+from datetime import datetime
+
+
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+local_server = True
+app = Flask(__name__)
+app.secret_key = 'super-secret-key'
+
+app.config.update(
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = '465',
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = params['gmail-user'],
+    MAIL_PASSWORD=  params['gmail-password']
+)
+mail = Mail(app)
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
+
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(21), nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    tagline = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    img_file = db.Column(db.String(12), nullable=True)
+
+
+@app.route("/")
+def home():
+    page = request.args.get('page', 1, type=int)
+    per_page = int(params['no_of_posts'])
+    posts = (
+        Posts.query.order_by(Posts.date.desc())  # Reverse order by date
+        .paginate(page=page, per_page=per_page, error_out=False)
+    )
+
+    last = math.ceil(posts.total / int(params['no_of_posts']))
+
+    page = request.args.get('page')
+    if (not str(page).isnumeric()):
+        page = 1
+    page = int(page)
+    posts = posts.items  # Extract the list of items from the Pagination object
+
+    # Pagination Logic
+    prev = None
+    next = None
+
+    if (page == 1):
+        prev = url_for('home', page=page)
+        if last > 1:
+            next = url_for('home', page=page + 1)
+    elif (page == last):
+        prev = url_for('home', page=page - 1)
+        next = url_for('home', page=page)
+    else:
+        prev = url_for('home', page=page - 1)
+        next = url_for('home', page=page + 1)
+
+    return render_template('index.html', params=params, posts=posts, prev=prev, next=next)
+
+
+
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, post=post)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', params=params)
+
+
+@app.route("/dashboard", methods=['GET', 'POST'])
+def dashboard():
+
+    if ('user' in session and session['user'] == params['admin_user']):
+        posts = Posts.query.all()
+        return render_template('dashboard.html', params=params, posts = posts)
+
+
+    if request.method=='POST':
+        username = request.form.get('uname')
+        userpass = request.form.get('pass')
+        if (username == params['admin_user'] and userpass == params['admin_password']):
+            #set the session variable
+            session['user'] = username
+            posts = Posts.query.all()
+            return render_template('dashboard.html', params=params, posts = posts)
+
+    return render_template('login.html', params=params)
+
+
+@app.route("/edit/<string:sno>", methods = ['GET', 'POST'])
+def edit(sno):
+    if ('user' in session and session['user'] == params['admin_user']):
+        if request.method == 'POST':
+            box_title = request.form.get('title')
+            tline = request.form.get('tline')
+            slug = request.form.get('slug')
+            content = request.form.get('content')
+            img_file = request.form.get('img_file')
+            date = datetime.now()
+
+            if sno=='0':
+                post = Posts(title=box_title, slug=slug, content=content, tagline=tline, img_file=img_file, date=date)
+                db.session.add(post)
+                db.session.commit()
+            else:
+                post = Posts.query.filter_by(sno=sno).first()
+                post.title = box_title
+                post.slug = slug
+                post.content = content
+                post.tagline = tline
+                post.img_file = img_file
+                post.date = date
+                db.session.commit()
+                return redirect('/edit/'+sno)
+        post = Posts.query.filter_by(sno=sno).first()
+        return render_template('edit.html', params=params, post=post, sno=sno)
+
+
+
+@app.route("/logout")
+def logout():
+    session.pop('user')
+    return redirect('/dashboard')
+
+@app.route("/delete/<string:sno>", methods = ['GET', 'POST'])
+def delete(sno):
+    if ('user' in session and session['user'] == params['admin_user']):
+        post = Posts.query.filter_by(sno=sno).first()
+        db.session.delete(post)
+        db.session.commit()
+    return redirect('/dashboard')
+
+
+
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        # Send an email to the admin
+        admin_msg = Message('New message from ' + name + ' (Admin Copy)',
+                            sender=email,
+                            recipients=[params['gmail-user']])
+        admin_msg.body = message + "\n" + phone
+        mail.send(admin_msg)
+
+        # Send a confirmation email to the user
+        user_msg = Message('Thank you for contacting us, ' + name,
+                           sender=params['gmail-user'],
+                           recipients=[email])
+        user_msg.body = "Thank you for reaching out to us. We'll get back to you as soon as possible."
+        mail.send(user_msg)
+
+        # Save the form data to the database
+        entry = Contacts(
+            name=name,
+            phone_num=phone,
+            msg=message,
+            date=datetime.now(),
+            email=email
+        )
+        db.session.add(entry)
+        db.session.commit()
+
+        return render_template('contact.html', params=params, success=True)
+
+    return render_template('contact.html', params=params, success=False)
+
+app.run(debug=True, host='192.168.0.127', port=80)
